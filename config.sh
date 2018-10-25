cf61228b4b83af46ee8f20701a47f163448dbcaa
liucz <liuchangzheng25@163.com>
2018-10-25 21:06:06 +0800

    jupyter ipython

--- config.sh
+++ config.sh
@@ -2,5 +2,5 @@
 git config --global user.name "liucz"
 git config --global user.email "liuchangzheng25@163.com"
 
-cp "./dotgit_config" "./dcmviewer/.git/config"
+cp "./dotgit_config" "./.git/config"
 source dvenv/Scripts/activate
