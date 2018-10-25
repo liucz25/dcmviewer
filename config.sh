54b23a4105ea56c4325bbbf225775b94cbdc7087
liucz <liuchangzheng25@163.com>
2018-10-25 20:59:41 +0800

    dvenv huanjing ok

---
+++ config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git config --global user.name "liucz"
+git config --global user.email "liuchangzheng25@163.com"
+
+cp "./dotgit_config" "./dcmviewer/.git/config"
+source dvenv/Scripts/activate
